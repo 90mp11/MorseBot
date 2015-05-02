bfe9058b84d6fdf491b07f7fc55dd9a32310339f
Matt Proctor <90mp11@gmail.com>
2015-05-02 16:26:28 +0100

    Refactor
    Small changes to how the name is called for
    Commented out sections of code to aid readability
    Added some intro text
    Got picky about how the command line variables are imported

--- MorseReciever.py
+++ MorseReciever.py
@@ -5,9 +5,6 @@ from pygame.locals import *
 from morse_lookup import *
 from twython import Twython
 
-pygame.mixer.pre_init(44100, -16, 1, 1024)
-pygame.init()
-
 #Twitter Calls to enable posting to the @getmorsebot account
 #pickle allows us to keep these keys secret!
 f = open('store.pckl')
@@ -19,9 +16,13 @@ CONSUMER_SECRET = t_key[1]
 ACCESS_KEY = t_key[2]
 ACCESS_SECRET = t_key[3]
 
-
+#Morse Code translation lengths
 NEXT_LETTER_LENGTH = 1.5
 NEXT_WORD_LENGTH = 4.5
+DASH_LENGTH = 0.15
+
+pygame.mixer.pre_init(44100, -16, 1, 1024)
+pygame.init()
 
 class ToneSound(pygame.mixer.Sound):
     def __init__(self, frequency, volume):
@@ -54,16 +55,24 @@ def post_tweet(message):
     print "Tweeting Complete!"
 
 def find_name():
+    global your_name 
     your_name = raw_input("Please enter your name: ")
     print "Thank you " + your_name
 
+def title_splash():
+    print "\n"
+    print "_  _ ____ ____ ____ ____ ___  ____ ___ "
+    print "|\/| |  | |__/ [__  |___ |__] |  |  |  "
+    print "|  | |__| |  \ ___] |___ |__] |__|  |  "
+    print "\n"
+
 def splash_screen(live_flag):
-	if (live_flag == "True"): #This flag will eventually allow for twitter/practise functionality
-		print "Ready to Tweet!"
-	else:
-		print "Ready to Test..."
-	find_name()
-	print "Please now transmit your message:"
+    if (live_flag == "True"): 
+        print "Ready to Tweet!"
+    else:
+    	print "Ready to Test..."
+    find_name()
+    print "Please now transmit your message:"
 
 def decoder_thread():
     global key_up_time
@@ -95,25 +104,35 @@ def decoder_thread():
 tone_obj = ToneSound(frequency = 800, volume = .5)
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 
-live_flag = sys.argv[1]
-message = 'Ready to Tweet!'
+#Test the command line arguements to set if we're in practise or live mode
+if (len(sys.argv) >= 2):
+    live_flag = sys.argv[1]
+else:
+    live_flag = False
 
+#Enable the pins that let us read the morse keyer
 pin = 7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+#Define a dot and dash in symbols
 DOT = "."
 DASH = "-"
 
+#variable definitions
 key_down_time = 0
 key_down_length = 0
 key_up_time = 0
 buffer = []
 output = [""]
+your_name = "Steve"
 
+#opening the translator thread and printing the Splash screen out to show that the program is ready
+title_splash()
 thread.start_new_thread(decoder_thread, ())
 splash_screen(live_flag)
 
+#Main program thread loops through translating the keyer's presses into either dit or dah
 while True:
     wait_for_keydown(pin)
     key_down_time = time.time() #record the time when the key went down
@@ -122,8 +141,9 @@ while True:
     key_up_time = time.time() #record the time when the key was released
     key_down_length = time.time() - key_down_time #get the length of time it was held down for
     tone_obj.stop()
-    buffer.append(DASH if key_down_length > 0.15 else DOT)
+    buffer.append(DASH if key_down_length > DASH_LENGTH else DOT)
 
+#    This code prints the dash or dot to the command line for debug
 #    if key_down_length > 0.15:
 #        print DASH
 #    else:

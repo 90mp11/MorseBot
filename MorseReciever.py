c1c3d9f08096ef261747d597461bac2e5129196c
Matt Proctor <90mp11@gmail.com>
2015-05-09 22:42:20 +0100

    Twitter Thread
    Now have the twitter details initialising in a separate thread as
    opposed to being called in the main program (slowing everything down)

--- MorseReciever.py
+++ MorseReciever.py
@@ -5,17 +5,6 @@ from pygame.locals import *
 from morse_lookup import *
 from twython import Twython
 
-#Twitter Calls to enable posting to the @getmorsebot account
-#pickle allows us to keep these keys secret!
-f = open('store.pckl')
-t_key = pickle.load(f)
-f.close()
-
-CONSUMER_KEY = t_key[0]
-CONSUMER_SECRET = t_key[1]
-ACCESS_KEY = t_key[2]
-ACCESS_SECRET = t_key[3]
-
 #Morse Code translation lengths
 NEXT_LETTER_LENGTH = 1.5
 NEXT_WORD_LENGTH = 4.5
@@ -23,6 +12,8 @@ DASH_LENGTH = 0.15
 
 pygame.mixer.pre_init(44100, -16, 1, 1024)
 pygame.init()
+
+api = None
 
 class ToneSound(pygame.mixer.Sound):
     def __init__(self, frequency, volume):
@@ -49,21 +40,33 @@ def wait_for_keyup(pin):
     while not GPIO.input(pin):
         time.sleep(0.01)
 
+def initiate_twitter_thread():
+    #Twitter Calls to enable posting to the @getmorsebot account
+    #pickle allows us to keep these keys secret!
+
+    f = open('store.pckl')
+    t_key = pickle.load(f)
+    f.close()
+
+    CONSUMER_KEY = t_key[0]
+    CONSUMER_SECRET = t_key[1]
+    ACCESS_KEY = t_key[2]
+    ACCESS_SECRET = t_key[3]
+
+    global api
+    api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
+
 def post_tweet(message):
     print "Tweeting your message now:"
+    global api
     api.update_status(status=message)
     print "Tweeting Complete!"
 
-def find_name():
-    global your_name 
-    your_name = raw_input("Please enter your name: ")
-    print "Thank you " + your_name
-
 def title_splash():
     print "\n"
-    print "_  _ ____ ____ ____ ____ ___  ____ ___ "
-    print "|\/| |  | |__/ [__  |___ |__] |  |  |  "
-    print "|  | |__| |  \ ___] |___ |__] |__|  |  "
+    print "_  _ ____ ____ ____ ____ ___  ____ ___      "
+    print "|\/| |  | |__/ [__  |___ |__] |  |  |       "
+    print "|  | |__| |  \ ___] |___ |__] |__|  |   v0.1"
     print "\n"
 
 def splash_screen(live_flag):
@@ -73,6 +76,11 @@ def splash_screen(live_flag):
     	print "Ready to Test..."
     find_name()
     print "Please now transmit your message:"
+
+def find_name():
+    global your_name 
+    your_name = raw_input("Please enter your name: ")
+    print "Thank you " + your_name
 
 def decoder_thread():
     global key_up_time
@@ -93,6 +101,7 @@ def decoder_thread():
                 print message
                 if (live_flag == "True"): 
                     post_tweet(message)
+                    find_name()
                 del output[:]
             del buffer[:]
         elif new_word and key_up_length >= NEXT_WORD_LENGTH:
@@ -102,13 +111,15 @@ def decoder_thread():
             sys.stdout.flush()
 
 tone_obj = ToneSound(frequency = 800, volume = .5)
-api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 
 #Test the command line arguements to set if we're in practise or live mode
 if (len(sys.argv) >= 2):
     live_flag = sys.argv[1]
 else:
     live_flag = False
+
+if (live_flag == "True"):
+    thread.start_new_thread(initiate_twitter_thread, ())
 
 #Enable the pins that let us read the morse keyer
 pin = 7

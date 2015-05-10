6d6f654c7718ad877d34fda8e7d2e15dd6b2991f
Matt Proctor <90mp11@gmail.com>
2015-05-10 01:11:02 +0100

    MorseBot v0.2
    Added exception handling for Keyboard Interrupt, allowing for graceful
    exiting of the program.
    Upon tweeting the message, the program will now "reset" asking for a new
    name
    User's name is now tweeted before their message too...

--- MorseReciever.py
+++ MorseReciever.py
@@ -13,8 +13,6 @@ DASH_LENGTH = 0.15
 pygame.mixer.pre_init(44100, -16, 1, 1024)
 pygame.init()
 
-api = None
-
 class ToneSound(pygame.mixer.Sound):
     def __init__(self, frequency, volume):
         self.frequency = frequency
@@ -42,7 +40,8 @@ def wait_for_keyup(pin):
 
 def initiate_twitter_thread():
     #Twitter Calls to enable posting to the @getmorsebot account
-    #pickle allows us to keep these keys secret!
+    #pickle allows us to keep these keys secret in the "store.pckl" file!
+    global api
 
     f = open('store.pckl')
     t_key = pickle.load(f)
@@ -53,20 +52,19 @@ def initiate_twitter_thread():
     ACCESS_KEY = t_key[2]
     ACCESS_SECRET = t_key[3]
 
-    global api
     api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 
 def post_tweet(message):
     print "Tweeting your message now:"
     global api
     api.update_status(status=message)
-    print "Tweeting Complete!"
+    print "Tweeting Complete!\n"
 
 def title_splash():
-    print "\n"
+    print ""
     print "_  _ ____ ____ ____ ____ ___  ____ ___      "
     print "|\/| |  | |__/ [__  |___ |__] |  |  |       "
-    print "|  | |__| |  \ ___] |___ |__] |__|  |   v0.1"
+    print "|  | |__| |  \ ___] |___ |__] |__|  |   v0.2"
     print "\n"
 
 def splash_screen(live_flag):
@@ -75,41 +73,48 @@ def splash_screen(live_flag):
     else:
     	print "Ready to Test..."
     find_name()
-    print "Please now transmit your message:"
+    print "\nPlease now transmit your message:"
 
 def find_name():
     global your_name 
     your_name = raw_input("Please enter your name: ")
-    print "Thank you " + your_name
+    print "Thank you " + your_name + "."
 
 def decoder_thread():
     global key_up_time
     global buffer
     global output
+    global live_flag
     new_word = False
     while True:
-        time.sleep(.01)
-        key_up_length = time.time() - key_up_time
-        if len(buffer) > 0 and key_up_length >= NEXT_LETTER_LENGTH:
-            new_word = True
-            bit_string = "".join(buffer)
-            output.append(try_decode(bit_string))
-            if (output[len(output) - 1] == 'EoT'):
-                output.remove('EoT')
-                print "\nThank you " + your_name + ". Your Message Follows:"
-                message = "".join(output)
-                print message
-                if (live_flag == "True"): 
-                    post_tweet(message)
-                    find_name()
-                del output[:]
-            del buffer[:]
-        elif new_word and key_up_length >= NEXT_WORD_LENGTH:
-            new_word = False
-            sys.stdout.write(" ")
-            output.append(" ")
-            sys.stdout.flush()
+        try:
+            time.sleep(.01)
+            key_up_length = time.time() - key_up_time
+            if len(buffer) > 0 and key_up_length >= NEXT_LETTER_LENGTH:
+                new_word = True
+                bit_string = "".join(buffer)
+                output.append(try_decode(bit_string))
+                if (output[len(output) - 1] == 'EoT'):
+                    output.remove('EoT')
+                    print "\nThank you " + your_name + ". Your Message Follows:"
+                    message = your_name + ": " + "".join(output)
+                    print message
+                    if (live_flag == "True"): 
+                        post_tweet(message)
+                        title_splash()
+                        splash_screen(live_flag)
+                    del output[:]
+                del buffer[:]
+            elif new_word and key_up_length >= NEXT_WORD_LENGTH:
+                new_word = False
+                sys.stdout.write(" ")
+                output.append(" ")
+                sys.stdout.flush()
+        except KeyboardInterrupt:
+            print "\nClosing Threads"
+            stated = False
 
+api = None
 tone_obj = ToneSound(frequency = 800, volume = .5)
 
 #Test the command line arguements to set if we're in practise or live mode
@@ -118,6 +123,7 @@ if (len(sys.argv) >= 2):
 else:
     live_flag = False
 
+#if the flagg to tweet is true, then we open a thread to handle the api 
 if (live_flag == "True"):
     thread.start_new_thread(initiate_twitter_thread, ())
 
@@ -143,17 +149,22 @@ title_splash()
 thread.start_new_thread(decoder_thread, ())
 splash_screen(live_flag)
 
+state_main = True
+
 #Main program thread loops through translating the keyer's presses into either dit or dah
 while True:
-    wait_for_keydown(pin)
-    key_down_time = time.time() #record the time when the key went down
-    tone_obj.play(-1) #the -1 means to loop the sound
-    wait_for_keyup(pin)
-    key_up_time = time.time() #record the time when the key was released
-    key_down_length = time.time() - key_down_time #get the length of time it was held down for
-    tone_obj.stop()
-    buffer.append(DASH if key_down_length > DASH_LENGTH else DOT)
-
+    try:
+        wait_for_keydown(pin)
+        key_down_time = time.time() #record the time when the key went down
+        tone_obj.play(-1) #the -1 means to loop the sound
+        wait_for_keyup(pin)
+        key_up_time = time.time() #record the time when the key was released
+        key_down_length = time.time() - key_down_time #get the length of time it was held down for
+        tone_obj.stop()
+        buffer.append(DASH if key_down_length > DASH_LENGTH else DOT)
+    except KeyboardInterrupt:
+        print "\nClosing Program"
+        break
 #    This code prints the dash or dot to the command line for debug
 #    if key_down_length > 0.15:
 #        print DASH
